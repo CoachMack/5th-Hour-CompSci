df34183d9ee57cdd1f3899ffdf91ecbed22b02b6
Coach Mack <jmckellips@wpsok.org>
2024-08-15 13:30:10 -0500

    Added PythonMath syntax notes

---
+++ PythonMath.py
@@ -0,0 +1,34 @@
+import math
+
+#Name: Coach Mack
+#Class: 5th Hour CS
+#Date: 08/15/2024
+
+#Addition
+add = 2 + 2
+
+#Subtraction
+subtract = 4 - 2
+
+#Multiplication
+multiply = 2 * 2
+
+#Division
+divide = 2 / 2
+
+#Exponent
+exp = 2 ** 2
+
+#Square Root
+sq = math.sqrt(4)
+
+#Rounding
+round = round(3.14)
+roundUp = math.ceil(3.14)
+roundDown = math.floor(3.14)
+
+x = int(input("Please Enter First Number: "))
+y = int(input("Please Enter Second Number: "))
+z = x + y
+
+print(z)
