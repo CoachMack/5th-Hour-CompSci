3e87b1cf5621b02dfb03d1550d17289225b700ce
Coach Mack <jmckellips@wpsok.org>
2024-08-21 12:50:13 -0500

    Added HW2 pseudo-code

---
+++ HW2.py
@@ -0,0 +1,12 @@
+#Name: Coach Mack
+#Class: 5th Hour
+#Assignment: HW2
+
+#Print Hello World!
+
+#Print a question asking for your name and a way for you to respond
+
+#Have it ask for two integers from the user
+
+#Add the two integers together and print them out
+
