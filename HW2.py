2c68f19098c5e5b1a317fca3a341ce34d01253e1
Coach Mack <jmckellips@wpsok.org>
2024-08-26 13:05:18 -0500

    Added MORE example notes to HW2

--- HW2.py
+++ HW2.py
@@ -5,10 +5,19 @@
 #Print Hello World!
 print("Hello World!")
 #Print a question asking for your name and a way for you to respond
-print("What is your name?")
-name = input()
+name = input(print("What is your name? "))
+nameTitle = name.title()
+nameUpper = name.upper()
+nameLower = name.lower()
 print("Hello", name)
+print("Hello", nameTitle)
+print("Hello", nameUpper)
+print("Hello", nameLower)
+
+print(f"Hello to {name}'s 5th Hour Class")
 #Have it ask for two integers from the user
-
+x = int(input())
+y = int(input())
 #Add the two integers together and print them out
-
+z = x + y
+print(f"The Sum is: {z}")
