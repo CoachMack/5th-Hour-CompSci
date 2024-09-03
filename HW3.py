fd4703d23412f421320c27cbd849c796e308c574
Coach Mack <jmckellips@wpsok.org>
2024-09-03 13:08:48 -0500

    Added optimizations and lecture notes to HW3

--- HW3.py
+++ HW3.py
@@ -1,4 +1,4 @@
-#Name:
+#Name: Coach Mack
 #Class: 5th Hour
 #Assignment: HW3
 #Code Used:
@@ -7,14 +7,26 @@
 #Objectives:
 
 #Print Hello World!
+print("Hello World!")
 
 #Create a list with 5 strings containing 5 different names in it
+nameList = ["Kevin", "Dom", "Zachy Poo", "Timmy", "Michael"]
 #Append a new name onto the Name List
+nameList.append("Bart")
 #Print out the 4th name on the list
+print(nameList[3])
+print(nameList[5])
 
 #Create a list with 4 different integers in it
+numList = [5, 2, 5000, 23]
 #Insert a new integer into the 2nd spot
+numList.insert(1, 56)
 #Print the Integer List
+print(numList)
 #Sort the list from lowest to highest
+numList.sort()
+print(numList)
 #Add the 1st three numbers on the sorted list together
+sum = numList[0]+numList[1]+numList[2]
 #Print the sum
+print(sum)
