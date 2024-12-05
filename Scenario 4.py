9b09a81ad94deef99fcb08aed687a7a9d2b4b8ee
Coach Mack <jmckellips@wpsok.org>
2024-12-05 13:17:17 -0600

    Added Notes for HW13, HW14, and Scenario 4

--- Scenario 4.py
+++ Scenario 4.py
@@ -1,4 +1,4 @@
-#Name:
+#Name: Coach Mack
 #Class: 5th Hour
 #Assignment: Scenario 4
 
@@ -10,3 +10,21 @@
 #They want to start prototyping the rating system and are asking you to make it.
 #This prototype needs to allow the user to input the number of players, let each player rate
 #a single model from 1 to 5, and then give the average score of all of the ratings.
+
+players = int(input("Enter number of players: "))
+i = 1
+ratingSum = 0
+
+if players < 2:
+    print("Error, not enough players")
+    exit()
+
+while i <= players:
+    rating = int(input("Enter rating from 1 to 5: "))
+    if rating > 5 or rating < 1:
+        print("Error, invalid rating.")
+    else:
+        ratingSum += rating
+        i += 1
+
+print("Average rating:", ratingSum / players)
