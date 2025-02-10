244c45a7afeeaabbc0ee8fe0dad7598d882e9db6
Coach Mack <jmckellips@wpsok.org>
2025-02-10 13:20:24 -0600

    Added HW18, HW19, and SC6 and 7 review notes

--- Scenario_6.py
+++ Scenario_6.py
@@ -8,8 +8,16 @@ import random
 #He wants you to make a function that rolls 4 six-sided dice (d6), sorts them from highest to lowest, and then adds the
 #highest 3 together. He then wants you to add that result to a list outside the function. He wants you to run that function
 #5 more times (six times total) and print all six stats.
+statblock = []
+#create a function
+def roll_dice():
+    for i in range(6):
+        roll = [random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)]
+        roll.sort(reverse=True)
+        stat = roll[0] + roll[1] + roll[2]
+        statblock.append(stat)
 
 #Once that is done, to ensure that the average of the statblock is fair (somewhere roughly between 12-13), he wants you
 #to plug it into a calculator (Scenario 7) and print the average.
-
-
+roll_dice()
+print(statblock)
